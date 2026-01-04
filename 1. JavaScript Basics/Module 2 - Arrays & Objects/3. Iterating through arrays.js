b7e971020d673bfc60329d01ad22e9eb7a1094a1
Daisy Kirui <97473233+DaisyKirui@users.noreply.github.com>
2026-01-05 00:48:18 +0300

    example

--- 1. JavaScript Basics/Module 2 - Arrays & Objects/3. Iterating through arrays.js
+++ 1. JavaScript Basics/Module 2 - Arrays & Objects/3. Iterating through arrays.js
@@ -1,1 +1,7 @@
 //iterating through an array using a for loop
+
+const fruits = ['apple', 'banana', 'orange', 'strawberry'];
+console.log(fruits)
+for (let i=0; i< fruits.length; i++){
+    console.log(fruits[i]); // accesses each element in the array and logs it onto the console
+}
