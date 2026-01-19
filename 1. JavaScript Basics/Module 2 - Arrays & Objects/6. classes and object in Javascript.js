43b7775b8b038826ace1c68528b8c9cd6e869498
Daisy Kirui <97473233+DaisyKirui@users.noreply.github.com>
2026-01-19 08:04:54 +0300

    objects

--- 1. JavaScript Basics/Module 2 - Arrays & Objects/6. classes and object in Javascript.js
+++ 1. JavaScript Basics/Module 2 - Arrays & Objects/6. classes and object in Javascript.js
@@ -1,1 +1,23 @@
 // classes and objects in Javascript
+// Classes - a blueprint or template for creating objects in JavaScript. It defines the structure and behaviour encapsulating data or properties and methods or functions that are common to a group of objects and bring an abstraction level to code. 
+// E.g a Person class, has properties such as name, and age, and methods such as sayHello. 
+
+// An Object, is a specific instance of a class created based on the blueprint and represents specific entities with unique values for properties defined in the class. 
+// e.g. for the person class, an object can be, a person named John, of age 30
+
+//creating a class in JavaScript:
+class Person {
+    constructor (firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+    getFullName (){
+        return `${this.firstName} ${this.lastName}`;
+    }
+}
+
+//creating an instance(object) of the Person Class
+const person_one = new Person ("John", "Doe")
+console.log(person_one.getFullName())
+
+// using object literals to create objects:
--- 1. JavaScript Basics/Module 2 - Arrays & Objects/Readme.md
+++ 1. JavaScript Basics/Module 2 - Arrays & Objects/Readme.md
@@ -14,3 +14,15 @@ An array is a data structure used to store a collection of values
 ### Working with 2D Arrays 
 
 Example: Movie Theatre Seating Arrangement
+
+# OBJECTS
+## Objects 
+
+#### Using Object Literals 
+
+#### Using FUnction Constructors
+
+##### Object Literals VS Function Constructors
+| ASPECT | OBJECT LITERALS | FUNCTION CONSTRUCTORS | 
+| ---|---|---|
+| Purpose | Suitable for ad hoc object | Ideal for creating multiple instances with shared properties and methods | 
